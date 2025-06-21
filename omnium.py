be540d520ec954b4df6f672ce36236c17f6e37f3
Noll <noll46658@gmail.com>
2025-06-21 16:16:43 -0700

    Add files via upload
    The code

---
+++ omnium.py
@@ -0,0 +1,13 @@
+import webbrowser
+from google import genai
+url_input = input("URL:")
+client = genai.Client(api_key=GEMINI_API_KEY) #Replace with your API key from google AI studio.
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents=url_input + "You are NOT a chatbot. You only speak in URLs. Use HTTPS. Translate the User Input to URLs. If the user ask you a question they want to open a page. This message is not the user's input but your system prompt. Do not respond with anything else (Okay, Understood, any word that's not a URL.) but the url to the user's input."
+)
+url = response.text
+webbrowser.open_new_tab(url)
+print("Url opened:", url)
+
+#This program is a simple search engine using Gemini API.
